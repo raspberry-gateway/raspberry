a1c3f18a2316ec294bb16e351c4f2492b9d81e1b
zhenli <zhenli@wacai.com>
2019-12-24 14:31:27 +0800

    Added api_definition_test.go file and will fill unittest func in the future

---
+++ tests/api_definition_test.go
@@ -0,0 +1,10 @@
+package main
+
+func createDefinition() ApiSpec {
+	// var thisDef = ApiDefinition{}
+	// var v1 = VersionInfo{}
+	// var thisSpec = ApiSpec{}
+	// var thisLoader = ApiDefinitionLoader{}
+
+	
+}
