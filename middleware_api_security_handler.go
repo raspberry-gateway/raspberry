b1416f4c7f042c354a98d53cf30e1aeb1ff94ccc
zhenli <zhenli@wacai.com>
2020-01-06 10:03:56 +0800

    Ducomenting

--- middleware_api_security_handler.go
+++ middleware_api_security_handler.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// CheckIsAPIOwner will ensure that the accessor of the Raspberry API has the correct security credentials - this is a
+// shared secret between the client and the owner and is set in the raspberry.conf file. This should never be made public!
 func CheckIsAPIOwner(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		raspberryAuthKey := r.Header.Get("X-Raspberry-Authorisation")
