0f9115ddeb83615f896dcfdcbb2751c2a2690be7
zhenli <zhenli@wacai.com>
2020-03-01 23:20:48 +0800

    Cleaning Raspberry project go files exclude main go file

--- tests/api_definition_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func createDefinition() APISpec {
-	// var thisDef = ApiDefinition{}
-	// var v1 = VersionInfo{}
-	// var thisSpec = ApiSpec{}
-	// var thisLoader = ApiDefinitionLoader{}
-
-}
